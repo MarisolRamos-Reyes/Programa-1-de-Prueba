b9466d3ebb3470943d1b0ed71f366ed936a0db74
MarisolRamos <71091704+MarisolRamos-Reyes@users.noreply.github.com>
2020-09-12 09:22:34 -0500

    Add files via upload

---
+++ Proyecto 1Prueba/Proyecto1 de Prueba/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proyecto1_de_Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Brandon ¿Como estas?");
+            Console.ReadKey();
+        }
+    }
+}
